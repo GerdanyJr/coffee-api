a63f377cf84e6259d737398afe53ed81eefe630d
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-15 17:40:43 -0300

    feat: :sparkles: create category endpoints

---
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Category;
+import com.example.demo.service.CategoryService;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        return ResponseEntity.ok(categoryService.getAllCategories());
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable("categoryId") Integer id) {
+        return ResponseEntity.ok(categoryService.getCategoryById(id));
+    }
+
+}
---
+++ src/main/java/com/example/demo/service/CategoryService.java
@@ -0,0 +1,27 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Category;
+import com.example.demo.repository.CategoryRepository;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategoryById(Integer id) {
+        return categoryRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("Categoria não encontrada com id " + id));
+    }
+}
