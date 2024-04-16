bd27a317a3173a7b3f4efe8fef629992732da02f
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-16 09:14:46 -0300

    build: :wrench: enable crossOrigin requests

--- src/main/java/com/example/demo/controller/CoffeeController.java
+++ src/main/java/com/example/demo/controller/CoffeeController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.example.demo.service.CoffeeService;
 
 @RestController
 @RequestMapping("/coffee")
+@CrossOrigin("${application.crossOriginUrl}")
 public class CoffeeController {
     private final CoffeeService coffeeService;
 
