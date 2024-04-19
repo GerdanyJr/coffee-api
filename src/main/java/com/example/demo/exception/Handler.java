75035f4eed9dc4efd416bd1400eda73bd5a58fb6
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-19 14:25:44 -0300

    fix: :bug: fix Handler Http status

--- src/main/java/com/example/demo/exception/Handler.java
+++ src/main/java/com/example/demo/exception/Handler.java
@@ -2,7 +2,6 @@ package com.example.demo.exception;
 
 import java.time.LocalDateTime;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,11 +10,10 @@ import com.example.demo.model.response.ErrorResponse;
 
 @RestControllerAdvice
 public class Handler {
-    
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundException(BaseException e) {
         return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
+                .status(e.getHttpStatus())
                 .body(new ErrorResponse(
                         e.getCode(),
                         e.getHttpStatus(),
