b1c0698da3ad9a16cd272004e55ac6a1da08d12c
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-19 14:23:35 -0300

    feat: :sparkles: implements userDetailsService

---
+++ src/main/java/com/example/demo/config/UserDetailsServiceConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceConfig implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário " + username + " não encontrado"));
+    }
+
+}
